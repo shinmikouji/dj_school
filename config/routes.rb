745996068f827b921dd0598008c49e49b9a51ee7
shinmikouji <sinkou83@gmail.com>
2021-02-12 13:41:27 +0900

    login-session

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   get '/', to: 'statics_pages#home'
   get '/top', to: 'statics_pages#top'
   get '/instructor', to: 'statics_pages#instructor'
-  get 'plan', to: 'statics_pages#plan'
-  get 'signup', to: 'users#new'
+  get '/plan', to: 'statics_pages#plan'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
